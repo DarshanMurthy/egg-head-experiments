8de81369e7a41c1a9e83bceab32a220605e24d81
Darshan Tumkur Sreenivasamurthy <darshants.darshan1@gmail.com>
2017-04-09 10:12:48 -0700

    incomplete test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+from 'react' import React
+from 'react-dom' import Reacts
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+class App expand React{
+  return ReactDOM= () => <h1> Hello </h1>
 }
-
-export default App;
