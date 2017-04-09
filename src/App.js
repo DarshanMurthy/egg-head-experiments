573b268ddff2c5255d655aa875c722ae3c80f28c
Darshan Tumkur Sreenivasamurthy <darshants.darshan1@gmail.com>
2017-04-09 10:36:13 -0700

    test output

---
+++ src/App.js
@@ -0,0 +1,6 @@
+from 'react' import React
+const App= () => <h1> Hello </h1>
+const Test= ()=> <h2> hello2 </h2>
+
+export default App
+export default Test
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+from 'react' import React
+from 'react-dom' import ReactDom
+from './App' import App
+from './App' import Test
 
+
+ReactDom.render(App{
+
+});
