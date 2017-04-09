45045b74077d975775d37d0211d0640f81102190
Darshan Tumkur Sreenivasamurthy <darshants.darshan1@gmail.com>
2017-04-09 16:35:56 -0700

    exp4

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
 import React from 'react'
-const App = () => <h1> Hello </h1>
-export default App;
+class App extends React.Component{
+  render() {
+    return <h1> Hello  Darshan you are awesome and smart</h1>
+}
+}
+
+
+export default App
