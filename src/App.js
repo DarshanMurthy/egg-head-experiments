6a5f2d914991f2cf884f235da0b2fdfae26da190
Darshan Tumkur Sreenivasamurthy <darshants.darshan1@gmail.com>
2017-04-09 16:24:41 -0700

    added the code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
-const App = () => <h1> Hello Apple </h1>
-const Test = () => <h2> Hello Test2 </h2>
-export default {App,Test}
+const App = () => <h1> Hello </h1>
+
+
+
+export default App;
