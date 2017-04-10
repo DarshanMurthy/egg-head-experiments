09aed3e8c93eb569da9f05ddc6026fec5fbde10f
Darshan Tumkur Sreenivasamurthy <darshants.darshan1@gmail.com>
2017-04-09 18:42:09 -0700

    added code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
+import React from 'react'
+class App extends React.Component{
+  render(){
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <div>
+      <h1> HelloTesst Darsha work hard like Kerservani </h1>
+    </div>
+)
   }
+
+
 }
 
-export default App;
+export default App
