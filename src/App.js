aee210aa50caaf17d7655bc779d05347b2945984
Darshan Tumkur Sreenivasamurthy <darshants.darshan1@gmail.com>
2017-04-11 10:58:04 -0700

    added temp

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 
 class App extends React.Components{
+  constructor(){
+    super();
+    this.state=
+    {
+      txt:"name sake values"
+    }
+
+  }
+
   render(){
-    return(<h1> Hello </h1>)
+    return(
+      <h1>
+        { this.state.txt}
+      </h1>
+    )
   }
 }
+
+
+export default App
