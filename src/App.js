95bdbb1c39a1f309f603ba32582641c11c3eaeba
Darshan Tumkur Sreenivasamurthy <darshants.darshan1@gmail.com>
2017-04-15 08:54:21 -0700

    temp

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react'
 import {Component} from 'react'
 
 class App extends Component{
+
+  const openNotification = notificationType =>{
+
+  }
+
   render(){
-    return(<h1> {this.props.txt}</h1>);
+    return(<h1> {this.props.txt} </h1>);
+
   }
 }
+
 
 export default App
